e2b1515ac8f32df12ea21d600dbed25b75b0b535
wtorniq <wtorniq@gmail.com>
2023-02-10 18:45:40 +0400

    refactor ChessView

--- app/src/main/java/com/example/heretochess/ui/main/ChessView.kt
+++ app/src/main/java/com/example/heretochess/ui/main/ChessView.kt
@@ -10,12 +10,10 @@ import java.lang.Math.abs
 
 class ChessView(context: Context, attributeSet: AttributeSet) : androidx.appcompat.widget.AppCompatImageView(context, attributeSet) {
 
+    private val paint = Paint()
+
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-
-
-        val paint = Paint()
-
         val margin = dpToPx(8)
         val squareSize = width / 8 - margin / 8
 
@@ -38,52 +36,16 @@ class ChessView(context: Context, attributeSet: AttributeSet) : androidx.appcomp
                 val rectRight = rectLeft + squareSize
                 val rectBottom = rectTop + squareSize
 
-                val rect = Rect(rectLeft, rectTop, rectRight, rectBottom)
+                val rect = makeRect(rectLeft,rectTop,rectRight,rectBottom)
 
                 canvas!!.drawRect(rect, paint)
             }
         }
     }
 
+    private fun makeRect(rectLeft: Int, rectTop: Int, rectRight: Int, rectBottom: Int) = Rect(rectLeft, rectTop, rectRight, rectBottom)
     private fun dpToPx(dp: Int): Int {
         return kotlin.math.abs(dp * resources.displayMetrics.density).toInt()
     }
 
-/*
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
-
-        val paint = Paint()
-        paint.color = Color.LTGRAY
-
-        val width = w - dpToPx(16)
-        val height = h - dpToPx(16)
-        val squareSize = if (width < height) width / 8 else height / 8
-        val margin = (width - 8 * squareSize) / 2
-        val left = margin
-        val top = (height - 8 * squareSize) / 2
-
-        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(bitmap)
-
-        for (i in 0 until 8) {
-            for (j in 0 until 8) {
-                if ((i + j) % 2 == 0) {
-                    val rect = Rect(
-                        left + i * squareSize,
-                        top + j * squareSize,
-                        left + (i + 1) * squareSize,
-                        top + (j + 1) * squareSize
-                    )
-                    canvas.drawRect(rect, paint)
-                }
-            }
-        }
-
-        setImageBitmap(bitmap)
-    }
-
-    private fun dpToPx(dp: Int): Int {
-        return (dp * resources.displayMetrics.density + 0.5f).toInt()
-    }*/
 }
